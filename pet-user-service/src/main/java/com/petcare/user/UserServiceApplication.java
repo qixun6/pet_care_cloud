d61b9d1cd97aaa40c267b1bb9d7e60f75f886396
qixun <2823632043@qq.com>
2026-05-18 20:32:50 +0800

    nacos配置

--- pet-user-service/src/main/java/com/petcare/user/UserServiceApplication.java
+++ pet-user-service/src/main/java/com/petcare/user/UserServiceApplication.java
@@ -2,10 +2,21 @@ package com.petcare.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class UserServiceApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(UserServiceApplication.class, args);
+    public static void main(String[] args) throws InterruptedException {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(UserServiceApplication.class, args);
+
+        /*while (true) {
+            String userName = applicationContext.getEnvironment().getProperty("user.name");
+            String userAge  = applicationContext.getEnvironment().getProperty("user.age");
+            String config = applicationContext.getEnvironment().getProperty("user.config");
+            System.out.println("userName: " + userName + ", userAge: " + userAge + ", config: " + config);
+            TimeUnit.SECONDS.sleep(1);
+        }*/
     }
 }
--- pet-user-service/src/main/java/com/petcare/user/controller/UserController.java
+++ pet-user-service/src/main/java/com/petcare/user/controller/UserController.java
@@ -3,6 +3,7 @@ package com.petcare.user.controller;
 import com.petcare.common.api.ApiResponse;
 import com.petcare.common.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,14 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
+@RefreshScope
 public class UserController {
 
     @Value("${server.port}")
     private String port;
 
+    @Value("${user.name}")
+    private String name;
+
     @GetMapping("/{id}")
     public ApiResponse<UserDTO> getUser(@PathVariable Long id) {
         UserDTO user = new UserDTO(id, "pet-owner-" + id + "-" + port, "13800000000");
+        return ApiResponse.ok(user);
+    }
+
+    @GetMapping("/refresh/{id}")
+    public ApiResponse<UserDTO> getRefreshScopeUser(@PathVariable Long id) {
+        UserDTO user = new UserDTO(id, name, "13800000000");
         return ApiResponse.ok(user);
     }
 }
